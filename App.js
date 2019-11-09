f2c5020354061efd45c5eb9faf37d6d0d6adcab3
Artem Golovin <artgolovin96@gmail.com>
2019-11-09 13:42:31 -0700

    Fix the styling to fill the screen

--- App.js
+++ App.js
@@ -11,10 +11,7 @@ const instructions = Platform.select({
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
     justifyContent: 'flex-end',
-    alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,

3d5d0476d44804c27a850218708f995821c38f86
Nathan Chua <natekchua@gmail.com>
2019-11-09 13:19:11 -0700

    fixed BottomBar

--- components/layout/BottomBar.js
+++ components/layout/BottomBar.js
@@ -1,41 +1,40 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import { BottomNavigation } from 'react-native-material-ui';
 
 class BottomBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'walk',
+    };
+  }
 
-    constructor() {
-        this.state = {
-            active: 'walk'
-        }
-    }
-    render() {
-        <BottomNavigation active={this.state.active} hidden={false}>
-            <BottomNavigation.Action
-                key="walk"
-                icon="walk"
-                label="walk"
-                onPress={() => this.setState({ active: 'walk' })}
-            />
-            <BottomNavigation.Action
-                key="car"
-                icon="car"
-                label="car"
-                onPress={() => this.setState({ active: 'car' })}
-            />
-            <BottomNavigation.Action
-                key="transit"
-                icon="transit"
-                label="transit"
-                onPress={() => this.setState({ active: 'transit' })}
-            />
-            <BottomNavigation.Action
-                key="bike"
-                icon="bike"
-                label="bike"
-                onPress={() => this.setState({ active: 'bike' })}
-            />
-        </BottomNavigation>
-    }
+  render() {
+    return (
+      <BottomNavigation active={this.state.active} hidden={false}>
+        <BottomNavigation.Action
+          key="walk"
+          icon="walk"
+          label="walk"
+          onPress={() => this.setState({ active: 'walk' })}
+        />
+        <BottomNavigation.Action key="car" icon="car" label="car" onPress={() => this.setState({ active: 'car' })} />
+        <BottomNavigation.Action
+          key="transit"
+          icon="transit"
+          label="transit"
+          onPress={() => this.setState({ active: 'transit' })}
+        />
+        <BottomNavigation.Action
+          key="bike"
+          icon="bike"
+          label="bike"
+          onPress={() => this.setState({ active: 'bike' })}
+        />
+      </BottomNavigation>
+    );
+  }
+}
 
-
-    export default BottomBar;
+export default BottomBar;
